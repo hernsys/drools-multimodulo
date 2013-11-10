9359779bf312c909a95f761b4b75ab3a789f7d94
hernsys <horacioantar@gmail.com>
2013-11-10 16:58:54 -0300

    add gwt module

---
+++ drools-main/target/.generated/com/hernsys/client/Messages_.java
@@ -0,0 +1,12 @@
+package com.hernsys.client;
+
+public class Messages_ implements com.hernsys.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
